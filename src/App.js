dba6080b58af2cc4df6c43f0d45a6eef0e943d60
Dario Kolak <kolak.dario@hotmail.com>
2022-06-19 17:25:55 +0200

    Create Subjects Service

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useContext } from "react";
 import AuthContext from "./store/auth-context";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "./pages/auth/Auth";
+import EditSubject from "./pages/subjects/EditSubject";
+import Subjects from "./pages/subjects/Subjects";
+import NewSubject from "./pages/subjects/NewSubject";
 
 const App = () => {
   const authCtx = useContext(AuthContext);
@@ -13,6 +16,24 @@ const App = () => {
           {authCtx.isUserLoggedIn && <Redirect to="/subjects" />}
           {!authCtx.isUserLoggedIn && <Redirect to="/auth" />}
         </Route>
+
+        {authCtx.isUserLoggedIn && (
+          <Route path="/subjects" exact>
+            <Subjects />
+          </Route>
+        )}
+
+        {authCtx.isUserLoggedIn && (
+          <Route path="/subjects/new-subject">
+            <NewSubject />
+          </Route>
+        )}
+
+        {authCtx.isUserLoggedIn && (
+          <Route path="/subjects/:subjectId/edit-subject">
+            <EditSubject />
+          </Route>
+        )}
 
         {!authCtx.isUserLoggedIn && (
           <Route path="/auth">
